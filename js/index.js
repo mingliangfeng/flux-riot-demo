b1f632476da5f4f83c71255983badc92857b383e
Andrew Feng <mingliangfeng@gmail.com>
2015-05-02 18:00:04 +1000

    add source code link

--- js/index.js
+++ js/index.js
@@ -1695,7 +1695,7 @@ require('./product/shop.tag')
 product_store = require('../stores/product_store.coffee')
 var flux_riot = require('flux-riot')
 
-riot.tag('app', '<div class="pure-g"> <div class="pure-u-1 pure-u-sm-3-5 list-grid"> <h1>flux-riot Shop Demo</h1> <shop></shop> </div> <div class="pure-u-1 pure-u-sm-2-5 cart-grid"> <cart></cart> </div> </div>', function(opts) {
+riot.tag('app', '<div class="pure-g"> <div class="pure-u-1 pure-u-sm-3-5 list-grid"> <h1>flux-riot Shop Demo</h1> <div class="source"><a href="https://github.com/mingliangfeng/flux-riot-demo">source code</a></div> <shop></shop> </div> <div class="pure-u-1 pure-u-sm-2-5 cart-grid"> <cart></cart> </div> </div>', function(opts) {
 
 });
 
